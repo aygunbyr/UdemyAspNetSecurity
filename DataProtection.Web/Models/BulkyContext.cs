3a0284e392e31a27e8f95102901e32afa0620177
Aygün Bayır <turkoaygun@gmail.com>
2024-10-06 15:18:48 +0300

    Add project files.

---
+++ DataProtection.Web/Models/BulkyContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DataProtection.Web.Models
+{
+    public partial class BulkyContext : DbContext
+    {
+        public BulkyContext()
+        {
+        }
+
+        public BulkyContext(DbContextOptions<BulkyContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasIndex(e => e.CategoryId);
+
+                entity.Property(e => e.Author).IsRequired();
+
+                entity.Property(e => e.Description).IsRequired();
+
+                entity.Property(e => e.ImageUrl)
+                    .IsRequired()
+                    .HasDefaultValueSql("(N'')");
+
+                entity.Property(e => e.Isbn)
+                    .IsRequired()
+                    .HasColumnName("ISBN");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DataProtection.Web/Models/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DataProtection.Web.Models
+{
+    public partial class Products
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public double ListPrice { get; set; }
+        public double Price { get; set; }
+        public double Price50 { get; set; }
+        public double Price100 { get; set; }
+        public int CategoryId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
